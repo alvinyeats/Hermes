a24cbcea6aec8d5a77db691ef262187ed3692b14
alvin <alvinyeats@gmail.com>
2018-02-08 00:07:34 +0800

    add args parse

--- scanner.py
+++ scanner.py
@@ -3,10 +3,36 @@
 # @author: Alvin
 # @create time: 2018.2.7
 
+# Python Lib
+import sys
+import getopt
 
-def main():
-    pass
 
+def main(argv):
+
+    system = ''
+    root = ''
+
+    try:
+        opts, args = getopt.getopt(argv, "hs:r:", ["system=", "root="])
+    except getopt.GetoptError as error:
+        print(error)
+        print('****** The correct format is: ******')
+        print('test.py -s <system> -r <root>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('****** Help Information ******')
+            print('test.py -s <system> -r <root>')
+            sys.exit()
+        elif opt in ("-s", "--system"):
+            system = arg
+        elif opt in ("-r", "--root"):
+            root = arg
+
+    print('system type: ', system)
+    print('root dir: ', root)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
